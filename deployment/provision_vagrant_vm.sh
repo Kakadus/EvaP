abd3d1a89aab747ddac767d39cdcf5f97dcce37f
Jonas Dittrich <58814480+Kakadus@users.noreply.github.com>
2023-12-05 07:34:09 +0100

    docker rootless fixes

--- deployment/provision_vagrant_vm.sh
+++ deployment/provision_vagrant_vm.sh
@@ -36,12 +36,10 @@ service redis-server restart
 # install apache
 apt-get -q install -y apache2 apache2-dev libapache2-mod-wsgi-py3
 
-# With docker for mac, root will own the mount point (uid=0). chmod does not touch the host file system in these cases.
-OWNER=$(stat -c %U "$MOUNTPOINT/evap")
-if [ "$OWNER" == "root" ]; then chown -R 1042 "$MOUNTPOINT"; fi
+# With docker for mac and docker rootless, root will own the mount point. Add --non-unique so that evap is an alias to root (uid=0) in this case
 
 # make user, create home folder, set uid to the same set in the Vagrantfile (required for becoming the synced folder owner), set default shell to bash
-useradd -m -u $(stat -c "%u" "$MOUNTPOINT/evap") -s /bin/bash evap
+useradd -m -u $(stat -c "%u" "$MOUNTPOINT/evap") -s /bin/bash evap --non-unique
 # allow ssh login
 cp -r /home/vagrant/.ssh /home/$USER/.ssh
 chown -R $USER:$USER /home/$USER/.ssh
