354262940bbd6103724c35b82c21176869aaa540
Jonas Dittrich <58814480+Kakadus@users.noreply.github.com>
2024-10-22 01:01:09 +0200

    type timeout as number

--- evap/static/ts/eslint.config.js
+++ evap/static/ts/eslint.config.js
@@ -48,7 +48,6 @@ export default tseslint.config(
                     ignoreRestSiblings: true,
                 },
             ],
-            "@typescript-eslint/no-redundant-type-constituents": "warn",
         },
     },
 );
