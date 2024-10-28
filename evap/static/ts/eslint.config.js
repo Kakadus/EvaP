5288cc73a0fcad97c55088dccb473a363568f9e3
Jonas Dittrich <58814480+Kakadus@users.noreply.github.com>
2024-10-28 18:51:55 +0100

    Move rules and make them a warning

--- evap/static/ts/eslint.config.js
+++ evap/static/ts/eslint.config.js
@@ -24,17 +24,6 @@ export default tseslint.config(
         rules: {
             "@typescript-eslint/restrict-template-expressions": ["error", { allowNumber: true }],
             "no-else-return": "error",
-            // not fixed in this PR
-            "@typescript-eslint/no-misused-promises": "off",
-            "@typescript-eslint/no-namespace": "off",
-            "@typescript-eslint/no-non-null-assertion": "off",
-            "@typescript-eslint/no-unsafe-call": "off",
-            "@typescript-eslint/no-unsafe-member-access": "off",
-            "@typescript-eslint/no-unsafe-argument": "off",
-            "@typescript-eslint/no-explicit-any": "off",
-            "@typescript-eslint/no-empty-function": "off",
-            "@typescript-eslint/no-unnecessary-type-parameters": "off",
-            // fixed in this PR
             "@typescript-eslint/no-unused-vars": [
                 "error",
                 {
@@ -47,6 +36,15 @@ export default tseslint.config(
                     ignoreRestSiblings: true,
                 },
             ],
+            "@typescript-eslint/no-empty-function": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unnecessary-type-parameters": "off",
+            "@typescript-eslint/no-misused-promises": "warn",
+            "@typescript-eslint/no-namespace": "warn",
+            "@typescript-eslint/no-non-null-assertion": "warn",
+            "@typescript-eslint/no-unsafe-call": "warn",
+            "@typescript-eslint/no-unsafe-member-access": "warn",
+            "@typescript-eslint/no-unsafe-argument": "warn",
         },
     },
 );
